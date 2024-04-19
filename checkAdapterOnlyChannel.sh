5047d3074f0e7c39d7f31f0154a959b9abd9877d
fufaich <fufaich@gmail.com>
2024-04-19 11:59:53 +0300

    Add the onlyChannel version

---
+++ checkAdapterOnlyChannel.sh
@@ -0,0 +1,94 @@
+#!/bin/bash
+
+sigHandler(){
+    ps aux | grep hostapd | awk '{if($1  == "root"){ print $2}}' | xargs sudo kill 2 2> /dev/null   
+    exit 0
+}
+
+getChannelsArrays(){
+    numPhy=$(iw dev $nameInterface info | grep wiphy | awk '{print $2}')
+    iw phy$numPhy info | sed -n '/Band 4:/q;p' | grep 'MHz \[' | grep -v "(disabled)" | cut -d ' ' -f4 | grep -o '[0-9]*' > num
+    mapfile -t array < num
+    rm num
+
+}
+
+createConfig(){
+    declare -A confList
+    confList["interface"]=$nameInterface
+    confList["hw_mode"]=$hw_mode
+    confList["channel"]=$ch
+    confList["driver"]=nl80211
+    confList["ssid"]=WiFiOnLinux
+
+    echo > $tmpConfig 
+    for key in "${!confList[@]}"; do
+        echo "$key=${confList[$key]}" >> $tmpConfig
+    done
+
+    unset confList
+}
+startStopAP(){
+    sleep $delay
+    hostapd -B -P $pid $tmpConfig > $tmp
+
+    res=$(grep "AP-DISABLED" $tmp)
+    if [[ $res ]] 
+        then
+            jsonObject+=" false ,"
+        else
+            jsonObject+=" true ,"
+    fi
+    # cat $tmp > $ch.log
+    cat $pid 2> /dev/null | xargs kill 2
+    rm $tmp
+    rm $tmpConfig
+}
+
+
+testChannel(){
+    if [[ $ch -gt 14 ]] 
+    then
+        hw_mode="a"
+#        ht_capab=("HT20" "HT40+" "HT40-")
+    else
+        hw_mode="g"
+#        ht_capab=("HT20" "HT40+" "HT40-")
+    fi
+
+
+    jsonObject+=" \"$ch\" :"
+    createConfig
+    startStopAP
+}
+
+mainLoop(){
+    for ch in ${array[@]} 
+    do
+        testChannel
+    done
+}
+
+
+
+
+
+############################################################
+nameInterface=$1
+resFile=$2
+tmpConfig=hostapdTmp.conf
+pid=hostapd.pid
+tmp="tmp.log"
+jsonObject=""
+delay=0.2
+declare -A ChannelsRes
+trap 'sigHandler' SIGINT
+
+jsonObject+="{"
+getChannelsArrays
+mainLoop
+
+jsonObject=${jsonObject%,} 
+jsonObject+=" }"
+echo $jsonObject > $resFile.json
+echo "Done" 
